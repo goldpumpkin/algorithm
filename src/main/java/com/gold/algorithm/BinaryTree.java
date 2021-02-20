9ab688ef064bfb5d94d955a3ca657dee0bc615d5
jin.huang <jin.huang@aihuishou.com>
2021-02-20 17:03:04 +0800

    二叉树的遍历

---
+++ src/main/java/com/gold/algorithm/BinaryTree.java
@@ -0,0 +1,134 @@
+package com.gold.algorithm;
+
+import com.gold.algorithm.pojo.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @author goldhuang
+ * @Description: 二叉树 - 多种遍历方式
+ *       1. 前序
+ *       2. 中序
+ *       3. 后序
+ *       4. 广度
+ *       5. 深度
+ *       6. 层
+ * @date 2021-02-19
+ */
+public class BinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.getInstance();
+
+        preorderTraversal(root);
+
+        System.out.println();
+        System.out.println("--------------------------");
+
+        inorderTraversal(root);
+
+        System.out.println();
+        System.out.println("--------------------------");
+
+        postorderTraversal(root);
+
+        System.out.println();
+        System.out.println("--------------------------");
+
+        nonRecursivePre(root);
+    }
+
+    /**
+     * 前序遍历：根节点在前面
+     * 遍历顺序：根节点 - 左子树 - 右子树
+     * @param root
+     */
+    public static void preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.getVal());
+        preorderTraversal(root.getLeft());
+        preorderTraversal(root.getRight());
+    }
+
+    /**
+     * 中序遍历：根节点在中间
+     * 遍历顺序：左子树 - 根节点 - 右子树
+     * @param root
+     */
+    public static void inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.getLeft());
+        System.out.print(root.getVal());
+        inorderTraversal(root.getRight());
+    }
+
+    /**
+     * 后序遍历：根节点在后面
+     * 遍历顺序：左子树 - 右子树- 根节点
+     * @param root
+     */
+    public static void postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal(root.getLeft());
+        postorderTraversal(root.getRight());
+        System.out.print(root.getVal());
+    }
+
+    /**
+     * 非递归 - 先序遍历
+     * @param root
+     */
+    public static void nonRecursivePre(TreeNode root) {
+        Stack<TreeNode> nodes = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !nodes.empty()) {
+            while (curr != null) {
+                System.out.print(curr.getVal());
+                nodes.push(curr);
+                curr = curr.getLeft();
+            }
+
+            if (!nodes.empty()) {
+                TreeNode treeNode = nodes.pop();
+                curr = treeNode.getRight();
+            }
+        }
+    }
+
+    /**
+     * 非递归 - 中序遍历
+     * @param root
+     */
+    public static void nonRecursiveIn(TreeNode root) {
+        Stack<TreeNode> nodes = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !nodes.empty()) {
+            while (curr != null) {
+                nodes.push(curr);
+                curr = curr.getLeft();
+            }
+
+            if (!nodes.empty()) {
+                TreeNode pop = nodes.pop();
+                System.out.println(pop.getVal());
+                curr = pop.getRight();
+            }
+        }
+    }
+
+    /**
+     * 非递归 - 后序遍历
+     * @param root
+     */
+    public static void nonRecursivePost(TreeNode root) {
+
+    }
+}
---
+++ src/main/java/com/gold/algorithm/pojo/TreeNode.java
@@ -0,0 +1,98 @@
+package com.gold.algorithm.pojo;
+
+import com.google.common.collect.Lists;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author goldhuang
+ * @Description: 二叉树 node
+ * @date 2021-02-19
+ */
+@Data
+public class TreeNode {
+
+    private Integer val;
+
+    private TreeNode left;
+
+    private TreeNode right;
+
+    public TreeNode(Integer val) {
+        this.val = val;
+    }
+
+    /**
+     * 构建一颗简单的完全二叉树 子节点可能包含 null
+     */
+    public static TreeNode getInstance(Integer[] data) {
+
+        // 1. 初始化 node
+        List<TreeNode> nodes = Lists.newLinkedList();
+        for (Integer i : data) {
+            nodes.add(i == null ? null : new TreeNode(i));
+        }
+
+        // 2. 组合 node
+        TreeNode root = nodes.get(0);
+        int nullNum = 0;
+        for (int i = 1; i < nodes.size(); i++) {
+            // 奇数
+            if (i % 2 != 0) {
+                int pIndex = (i - 1) / 2 + nullNum;
+                TreeNode p = nodes.get(pIndex);
+                p.setLeft(nodes.get(i));
+            } else {
+                int pIndex = (i - 2) / 2 + nullNum;
+                TreeNode p = nodes.get(pIndex);
+                p.setRight(nodes.get(i));
+
+                if (nodes.get(i - 1) == null) {
+                    nullNum++;
+                }
+            }
+        }
+        return root;
+    }
+
+    /**
+     * 构建一颗简单的完全二叉树 子节点可能包含 null
+     */
+    public static TreeNode createBinaryTreeWithNonNull(int[] data, int index) {
+        TreeNode node = null;
+        if (index >= data.length) {
+            return node;
+        }
+
+        int val = data[index];
+        node = new TreeNode(val);
+        node.left = createBinaryTreeWithNonNull(data, 2 * index + 1);
+        node.right = createBinaryTreeWithNonNull(data, 2 * index + 2);
+
+        return node;
+    }
+
+    /**
+     *          1
+     *        /  \
+     *       2    3
+     *     / \   / \
+     *    4   5 n   7
+     *
+     * @return
+     */
+    public static TreeNode getInstance() {
+        Integer[] data = {1, 2, 3, 4, 5, null, 7};
+        return getInstance(data);
+    }
+
+    public static void main(String[] args) {
+        TreeNode instance = getInstance();
+        System.out.println(instance);
+
+//        int[] data = {1,2,3,4,5,6,7};
+//        TreeNode root = createBinaryTreeWithNonNull(data, 0);
+//        System.out.println(root);
+    }
+}
