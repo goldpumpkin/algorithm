738045c2a7415a345d5c9e0739bcd56673d0173b
jin.huang <jin.huang@aihuishou.com>
2021-03-13 11:23:18 +0800

    container with most warter

--- src/main/java/com/gold/algorithm/leetcode/ContainerWithMostWater11.java
+++ src/main/java/com/gold/algorithm/leetcode/ContainerWithMostWater11.java
@@ -7,8 +7,27 @@ package com.gold.algorithm.leetcode;
  */
 public class ContainerWithMostWater11 {
 
+    /**
+     * 思路：
+     * 1. 问题是 找出最大的面积
+     * 2. 面积 = min(h1,h2) * (j-i)
+     * 3. 以宽度最大为基准，只有移动高度较小的边，才有可能使得面积变大
+     */
     public int maxArea(int[] height) {
-        return 0;
+        int maxArea = 0;
+        int l = 0, r = height.length - 1;
+
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            maxArea = Math.max(maxArea, area);
+
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxArea;
     }
 
 }
