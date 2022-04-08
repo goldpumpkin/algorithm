b7fef5a5103305eeea57e241f1ecd177d2dd2cfb
jin.huang <jin.huang@aihuishou.com>
2022-04-09 00:04:22 +0800

    ValidPalindrome_125

--- src/main/java/com/gold/algorithm/leetcode/ValidPalindrome_125.java
+++ src/main/java/com/gold/algorithm/leetcode/ValidPalindrome_125.java
@@ -1,6 +1,32 @@
 package com.gold.algorithm.leetcode;
 
-public class StringisPalindrome_125 {
+public class ValidPalindrome_125 {
 
+    /**
+     * https://leetcode.com/problems/valid-palindrome/
+     */
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(i)) - Character.toLowerCase(s.charAt(j)) != 0) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
 
 }
