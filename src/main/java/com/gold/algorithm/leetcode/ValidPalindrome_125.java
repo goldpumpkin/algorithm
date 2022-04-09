b3831b4d4d4517b7829d10d6dae364b2515ec7a6
jin.huang <jin.huang@aihuishou.com>
2022-04-09 18:06:42 +0800

    ValidPalindrome_125

--- src/main/java/com/gold/algorithm/leetcode/ValidPalindrome_125.java
+++ src/main/java/com/gold/algorithm/leetcode/ValidPalindrome_125.java
@@ -1,5 +1,7 @@
 package com.gold.algorithm.leetcode;
 
+import com.gold.algorithm.model.ListNode;
+
 public class ValidPalindrome_125 {
 
     /**
@@ -24,6 +26,114 @@ public class ValidPalindrome_125 {
 
             i++;
             j--;
+        }
+
+        return true;
+    }
+
+    /**
+     * 回文 - 单向链表
+     * 可以看看别人的代码：https://github.com/andavid/leetcode-java/blob/master/note/234/README.md
+     * 比我的简单
+     */
+    public static boolean isPalindrome(ListNode<String> head) {
+        if (head == null) {
+            return false;
+        }
+
+        if (head.getNext() == null) {
+            return true;
+        }
+
+        ListNode<String> prev = null;
+        ListNode<String> slow = head;
+        ListNode<String> fast = head;
+
+        while (fast.getNext() != null && fast.getNext().getNext() != null) {
+            fast = fast.getNext().getNext();
+
+            final ListNode<String> next = slow.getNext();
+            slow.setNext(prev);
+            prev = slow;
+            slow = next;
+        }
+
+        if (fast.getNext() != null && prev != null) {
+            slow.setNext(prev);
+            prev = slow;
+            slow = slow.getNext();
+        }
+
+        if (prev == null) {
+            prev = head;
+            slow = slow.getNext();
+        }
+
+        while (prev != null && slow.getNext() != null) {
+            final String d1 = prev.getData();
+            final String d2 = slow.getNext().getData();
+            if (!d1.equals(d2)) {
+                return false;
+            }
+
+            prev = prev.getNext();
+            slow = slow.getNext();
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        final ListNode<String> palindrome = ListNode.buildPalindrome();
+        System.out.println(palindrome);
+
+        final boolean p = isPalindrome(palindrome);
+        System.out.println(p);
+
+        final boolean p2 = isPalindrome2(palindrome);
+        System.out.println(p2);
+    }
+
+    /**
+     * 区别于上一个方法：指针初始化时，已经开始走了一步
+     */
+    public static boolean isPalindrome2(ListNode<String> head) {
+        if (head == null) {
+            return false;
+        }
+
+        if (head.getNext() == null) {
+            return true;
+        }
+
+        ListNode<String> prev = head;
+        ListNode<String> slow = head;
+        ListNode<String> fast = head.getNext();
+
+        while (fast.getNext() != null && fast.getNext().getNext() != null) {
+            fast = fast.getNext().getNext();
+
+            final ListNode<String> next = slow.getNext();
+            slow.setNext(prev);
+            prev = slow;
+            slow = next;
+        }
+
+        if (fast.getNext() != null) {
+            slow.setNext(prev);
+            prev = slow;
+            slow = slow.getNext();
+        }
+
+        while (prev != null && slow.getNext() != null) {
+            final String d1 = prev.getData();
+            final String d2 = slow.getNext().getData();
+            if (!d1.equals(d2)) {
+                return false;
+            }
+
+            prev = prev.getNext();
+            slow = slow.getNext();
         }
 
         return true;
--- src/main/java/com/gold/algorithm/model/ListNode.java
+++ src/main/java/com/gold/algorithm/model/ListNode.java
@@ -1,2 +1,62 @@
-package com.gold.algorithm.model;public class ListNode {
+package com.gold.algorithm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ListNode<T> {
+
+    private T data;
+
+    private ListNode<T> next;
+
+    public static ListNode<String> buildPalindrome() {
+        final String s = "rr";
+        return buildSinglyLinkedList(s);
+    }
+
+    public static ListNode<String> buildSinglyLinkedList(String s) {
+        final char[] chars = s.toCharArray();
+
+        ListNode<String> head = null;
+        ListNode<String> prev = null;
+        for (char c : chars) {
+            final String d = Character.valueOf(c).toString();
+            final ListNode<String> node = ListNode.<String>builder()
+                .data(d)
+                .build();
+
+            if (prev == null) {
+                head = prev = node;
+                continue;
+            }
+
+            prev.setNext(node);
+            prev = node;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode<String> stringListNode = buildPalindrome();
+        System.out.println(stringListNode);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder s = new StringBuilder();
+        ListNode<String> head = (ListNode<String>) this;
+        while (head != null) {
+            s.append(head.getData());
+            head = head.next;
+        }
+
+        return s.toString();
+    }
 }
